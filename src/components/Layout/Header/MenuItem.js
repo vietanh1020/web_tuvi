06249969057e364096c1a32ad4494f713d9d5cbf
AnhVV <anh.vv@zinza.com.vn>
2025-01-03 23:00:49 +0700

    add icon

--- src/components/Layout/Header/MenuItem.js
+++ src/components/Layout/Header/MenuItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Icon from '../../common/Icon';
 
 const MenuItem = ({ icon, title, url }) => {
     return (
-        <Link to={url} className="mx-4 text-primary  py-3 text-lg font-medium border-b border-[#913f33] block">
-            <span className="text-xl mr-3">{icon}</span>
+        <Link
+            to={url}
+            className="mx-8 text-primary  py-3 text-lg font-medium border-b border-[#913f33] flex items-center gap-3"
+        >
+            <Icon name={icon} size={24} />
             <span className="font-medium">{title}</span>
         </Link>
     );
--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -5,29 +5,35 @@ const menu = [
     {
         title: 'Trang chủ',
         url: '/',
-        icon: '',
+        icon: 'Home',
     },
 
     {
         title: 'Lập lá số',
         url: '/',
-        icon: '',
+        icon: 'FileText',
     },
 
     {
         title: ' Danh sách lá số đã lưu',
         url: '/',
-        icon: '',
+        icon: 'Scroll',
     },
+    {
+        title: 'Mua gói VIP',
+        url: '/',
+        icon: 'Tickets',
+    },
+
     {
         title: 'Điều khoản chính sách',
         url: '/',
-        icon: '',
+        icon: 'Flag',
     },
     {
         title: 'Giới thiệu',
         url: '/',
-        icon: '',
+        icon: 'Info',
     },
 ];
 
@@ -40,7 +46,7 @@ const Header = () => {
 
     return (
         <header className="bg-[#760F00] shadow-md fixed w-full z-50 top-0 left-0">
-            <div className="container mx-auto  flex justify-between items-center">
+            <div className="px-2 flex justify-between items-center">
                 <div className="">
                     <img src="asset/images/logo-mobile.png" height={60}></img>
                 </div>
@@ -59,7 +65,7 @@ const Header = () => {
                 </nav>
 
                 {/* Mobile Menu Button */}
-                <button className="md:hidden  focus:outline-none" onClick={toggleMenu}>
+                <button className="md:hidden  focus:outline-none mr-2" onClick={toggleMenu}>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="h-6 w-6"
@@ -80,7 +86,7 @@ const Header = () => {
             >
                 <div
                     className="flex justify-between
-                 border-b-2 border-[#f5d556] items-center pl-2 pr-4"
+                 border-b border-[#f5d556] items-center pl-2 pr-4"
                 >
                     <div className="text-2xl font-bold">
                         <a href="#">
@@ -93,7 +99,7 @@ const Header = () => {
                             className="h-6 w-6"
                             fill="none"
                             viewBox="0 0 24 24"
-                            stroke="currentColor"
+                            stroke="white"
                         >
                             <path
                                 strokeLinecap="round"
